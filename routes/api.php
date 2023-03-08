fac293efbe4886e3d6d85224214d9940fe5b2e68
mostafa123c <mostafaemad112744@gmail.com>
2023-03-08 14:00:52 +0200

    ImageRoutes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\V1\AlbumController;
-use App\Http\Controllers\V1\ImageManipulationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,21 +9,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-});
-
-
-Route::prefix('v1')->group(function() {
-    Route::apiResource('album', AlbumController::class);
-    Route::get('image', [ImageManipulationController::class, 'index']);
-    Route::get('image/by-album/{album}', [ImageManipulationController::class, 'byAlbum']);
-    Route::get('image/{image}', [ImageManipulationController::class, 'show']);
-    Route::post('image/resize', [ImageManipulationController::class, 'resize']);
-    Route::delete('image/{image}', [ImageManipulationController::class, 'destroy']);
 });
